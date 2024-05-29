8c5c89405f1155ae6f89aeadf683cec3c6020a98
ekrispy <estevan.crispin@gmail.com>
2024-05-29 11:49:35 -0600

    created user route, model,and controller

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const posts = require("./routes/posts");
+const users = require("./routes/users");
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// add middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+  });
+  
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.use("/posts", posts);
+app.use("/api/posts", posts);
+app.use("/api/users", users);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+
+mongoose.connect(process.env.Atlas_URI)
+ .then(() => {
+    // Listen for requests
+    const port = process.env.PORT || 3000; // default port if PORT is not set
+    app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`);
+    });
+  })
+ .catch((error) => {
+    console.error(`Error connecting to DB: ${error.message}`); // log error with message
+    process.exit(1); // exit with error code if DB connection fails
+  });
