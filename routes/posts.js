bba011523699734438b06c844ad7993a54d28fcc
ekrispy <estevan.crispin@gmail.com>
2024-05-29 15:10:12 -0600

    added post routes, post schema and postcontroller

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const  { getAllPosts, getSinglePost } = require("../controllers/postcontroller");
+const  { getAllPosts, getSinglePost, createPost } = require("../controllers/postcontroller");
 
 
 router.get("/", getAllPosts);
 
 router.get("/:id", getSinglePost);
+
+router.post("/", createPost);
+
+
 
 module.exports = router
