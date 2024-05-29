3e7168d5eaed8b75404df9a14510b8e289ccdad5
ekrispy <estevan.crispin@gmail.com>
2024-05-29 11:27:34 -0600

    added models, controllers, and routes folder

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+const posts = require("./routes/posts");
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/posts", posts);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
