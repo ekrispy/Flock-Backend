43125a6e3f48d2655690e1971fb6a03a4089ddd9
ekrispy <estevan.crispin@gmail.com>
2024-05-29 15:10:12 -0600

    added post routes, post schema and postcontroller

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const Post = require("../models/Post");
+const Post = require("../models/postsSchema");
 
 
 // get all posts
@@ -23,4 +23,16 @@ const getSinglePost = async (req, res) => {
     }
 }
 
-module.exports = { getAllPosts, getSinglePost }
+// create new post
+const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new Post({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+module.exports = { getAllPosts, getSinglePost, createPost }
--- models/postsSchema.js
+++ models/postsSchema.js
@@ -4,4 +4,8 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    UserId: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model("Post", postSchema);
