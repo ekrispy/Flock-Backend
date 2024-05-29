21443898800cf18787b9d70ca1b47a67ffcf3667
ekrispy <estevan.crispin@gmail.com>
2024-05-29 13:28:30 -0600

    updated user schema, and added salt and hash to create unique password in mongodo

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
-const express = require("express");
-
+const express = require('express');
 const router = express.Router();
+const { getAllUsers, getSingleUser, createUser, loginUser } = require('../controllers/UserController');
 
-const { getAllUsers, getSingleUser, createUser } = require("../controllers/UserController");
+// Route to get all users
+router.get('/', getAllUsers);
 
+// Route to get a single user by ID
+router.get('/:id', getSingleUser);
 
-router.get("/", getAllUsers);
+// Route to create a new user
+router.post('/register', createUser);
 
-router.get("/:id", getSingleUser);
-
-router.post("/", createUser);
+// Route to login user
+router.post('/login', loginUser);
 
 module.exports = router;
